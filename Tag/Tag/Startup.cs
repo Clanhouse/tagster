639227d1daa680c084da89fd9af11b6fa193336f
Lukasz Pajak <pajakukasz@gmail.com>
2021-04-29 20:30:01 +0200

    TAG-18: Delete comments

--- Tag/Tag/Startup.cs
+++ Tag/Tag/Startup.cs
@@ -20,13 +20,12 @@ namespace Tag
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+     
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +35,7 @@ namespace Tag
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               
                 app.UseHsts();
             }
 
