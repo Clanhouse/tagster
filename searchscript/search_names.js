8600b65451e4d2bc7992a650dba58563d5fbef92
Paweł Sitarz <pawel.sitarz98@gmail.com>
2021-05-04 22:46:47 +0200

    TAG14- Download all href profiles on site

--- searchscript/search_names.js
+++ searchscript/search_names.js
@@ -1,3 +1,12 @@
-var paragraphCount = document.evaluate( 'count(//p)', document, null, XPathResult.ANY_TYPE, null );
-
-alert( 'This document contains ' + paragraphCount.numberValue + ' paragraph elements' );
+xpath = "//a[@class='app-aware-link feed-shared-actor__container-link relative display-flex flex-grow-1']/@href";
+parent = document;
+function GetPersonID(xpath, parent)
+{ 
+    let results = [];
+    let query = document.evaluate(xpath,parent,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+    for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+        results.push(query.snapshotItem(i));
+    }
+    return results;
+  
+}
