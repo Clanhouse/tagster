0fdc7ddb41198d03b150e97b1b30f485200f5f1d
Lukasz Pajak <pajakukasz@gmail.com>
2021-05-15 16:12:57 +0200

    TAG-16: Create a database, two table with One-To-Many relation

---
+++ TagsterWebAPI/Tagster.Database/Profile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Tagster.Database
+{
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Tags> Tags { get; set; }
+    }
+}
---
+++ TagsterWebAPI/Tagster.Database/Tags.cs
@@ -0,0 +1,10 @@
+﻿namespace Tagster.Database
+{
+    public class Tags
+    {
+        public int Id { get; set; }
+        public string TagName { get; set; }
+        public Profile Profile { get; set; }
+        public int ProfileId { get; set; }
+    }
+}
