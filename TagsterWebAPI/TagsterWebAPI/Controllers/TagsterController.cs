3341c8fb9a2adca09ac41a4304aefa63cbe4960b
Lukasz Pajak <pajakukasz@gmail.com>
2021-05-15 13:44:16 +0200

    TAG-18: New Web API project and default GET.

---
+++ TagsterWebAPI/TagsterWebAPI/Controllers/TagsterController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagsterWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TagsterController : ControllerBase
+    {
+        private static readonly string[] Tags = new[]
+        {
+            "pOLAK", "Donkey", "Nice guy"
+        };
+
+        private readonly ILogger<TagsterController> _logger;
+
+        public TagsterController(ILogger<TagsterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Tagster> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Tagster
+            {
+                Terms = Tags[rng.Next(Tags.Length)]
+                
+                
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ TagsterWebAPI/TagsterWebAPI/Tagster.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TagsterWebAPI
+{
+    public class Tagster
+    {
+        public string Terms { get; set; }
+    }
+}
