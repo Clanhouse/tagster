63a90672eba7973e17404c938fb52f8783d9d80b
flapek <filap4@gmail.com>
2021-07-17 10:51:24 +0200

    create repository and

---
+++ TagsterWebAPI/src/Tagster.Domain/Repositories/ITagsRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+namespace Tagster.Domain.Repositories
+{
+    public interface ITagsRepository
+    {
+        /// <summary>
+        /// Get tag or tags
+        /// </summary>
+        /// <returns></returns>
+        public Task<object> GetAsync();
+        /// <summary>
+        /// Add tag
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> AddAsync();
+        /// <summary>
+        /// Update tag
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> UpdateAsync();
+        /// <summary>
+        /// Delete tag
+        /// </summary>
+        /// <returns></returns>
+        public Task<bool> DeleteAsync();
+
+    }
+}
