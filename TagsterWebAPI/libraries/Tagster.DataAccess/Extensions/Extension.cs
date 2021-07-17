1dea03ffd854b4abd2a686a9a55e57a0931506e1
flapek <filap4@gmail.com>
2021-07-17 10:52:08 +0200

    move initializaion of db context to extension method

---
+++ TagsterWebAPI/libraries/Tagster.DataAccess/Extensions/Extension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Tagster.DataAccess.DBContexts;
+
+namespace Tagster.DataAccess.Extensions
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, string connectionString)
+            => services.AddDbContext<TagsterDbContext>(options => options
+            .UseSqlServer(connectionString))
+            .AddScoped<ITagsterDbContext, TagsterDbContext>();
+    }
+}
