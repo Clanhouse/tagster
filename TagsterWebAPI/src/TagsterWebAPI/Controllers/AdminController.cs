c5ef67e6a9881c6a6e43dea05248bed17477b703
kacperantoniak <kacper.antoniak08@gmail.com>
2021-08-19 05:00:48 +0200

    Add AdminController, Generated data utils and Admin interface. Implementation of test data injection to database. IN PROGESS

---
+++ TagsterWebAPI/src/TagsterWebAPI/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tagster.Application.Services;
+using Tagster.DataAccess.Entities;
+
+namespace TagsterWebAPI.Controllers
+{
+    [Route("admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+            => _adminService = adminService;
+
+        [HttpGet]
+        [Route("generate-fake-data")]//add variables to route! (profilesCount, maxTagsPerProfile)
+        //[ProducesResponseType(typeof(ICollection<Tag>[]), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GenFakeData(int profilesCount, int maxTagsPerProfile)
+        {
+            await _adminService.CreateFakeData(profilesCount, maxTagsPerProfile);
+            return Ok();
+        }
+
+    }
+}
