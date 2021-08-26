03c74c18313edfe5acb57310bdd65f3dbeae48b0
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-08-26 18:48:15 +0200

    View has been removed. Required Password Email Adress and Password Confirmation.

--- TagsterWebAPI/src/TagsterWebAPI/Models/RegisterViewModel.cs
+++ TagsterWebAPI/src/TagsterWebAPI/Models/RegisterViewModel.cs
@@ -17,10 +17,9 @@ namespace TagsterWebAPI.Models
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
+            [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password",
-                ErrorMessage = "Password and confirmation password do not match.")]
+            
             public string ConfirmPassword { get; set; }
         
 
