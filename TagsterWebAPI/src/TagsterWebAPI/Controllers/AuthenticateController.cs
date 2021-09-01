5390c229d978fb9fba87e2d9af3ae2978d61e510
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-09-02 00:08:22 +0200

    Initial commit after update

--- TagsterWebAPI/src/TagsterWebAPI/Controllers/AuthenticateController.cs
+++ TagsterWebAPI/src/TagsterWebAPI/Controllers/AuthenticateController.cs
@@ -10,8 +10,6 @@ using System.Threading.Tasks;
 using TagsterWebAPI.Models;
 
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace TagsterWebAPI.Controllers
 {
     [Authorize]
