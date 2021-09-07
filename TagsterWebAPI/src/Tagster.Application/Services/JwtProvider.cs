61f71dedca44f74fae3e0beabef71ade7dc6018c
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 13:03:05 +0200

    Update JwtProvider.cs

--- TagsterWebAPI/src/Tagster.Application/Services/JwtProvider.cs
+++ TagsterWebAPI/src/Tagster.Application/Services/JwtProvider.cs
@@ -11,15 +11,9 @@ namespace Tagster.Application.Services
         public JwtProvider(IJwtHandler jwtHandler) 
             => _jwtHandler = jwtHandler;
 
-<<<<<<< HEAD
-        public AuthDto Create(Guid userId, string email)
-        {
-            var jwt = _jwtHandler.CreateToken(userId.ToString(), email);
-=======
         public AuthDto Create(int userId, string email)
         {
             var jwt = _jwtHandler.CreateToken(userId, email);
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
 
             return new AuthDto
             {
