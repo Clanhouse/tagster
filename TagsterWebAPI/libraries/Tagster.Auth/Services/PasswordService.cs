a9d84b5fa72af251e597cfa04039887e24d4d835
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 13:09:59 +0200

    Update PasswordService.cs

--- TagsterWebAPI/libraries/Tagster.Auth/Services/PasswordService.cs
+++ TagsterWebAPI/libraries/Tagster.Auth/Services/PasswordService.cs
@@ -6,22 +6,6 @@ namespace Tagster.Auth.Services
     {
         private readonly IPasswordHasher<IPasswordService> _passwordHasher;
 
-<<<<<<< HEAD
-        public PasswordService(IPasswordHasher<IPasswordService> passwordHasher)
-        {
-            _passwordHasher = passwordHasher;
-        }
-
-        public bool IsValid(string hash, string password)
-        {
-            return _passwordHasher.VerifyHashedPassword(this, hash, password) != PasswordVerificationResult.Failed;
-        }
-
-        public string Hash(string password)
-        {
-            return _passwordHasher.HashPassword(this, password);
-        }
-=======
         public PasswordService(IPasswordHasher<IPasswordService> passwordHasher) 
             => _passwordHasher = passwordHasher;
 
@@ -30,6 +14,5 @@ namespace Tagster.Auth.Services
 
         public string Hash(string password) 
             => _passwordHasher.HashPassword(this, password);
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
     }
 }
