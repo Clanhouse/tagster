4b035f02cfbb96ca657f35af8be3c49617f8bdd5
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 13:10:19 +0200

    Update JsonWebToken.cs

--- TagsterWebAPI/libraries/Tagster.Auth/Models/JsonWebToken.cs
+++ TagsterWebAPI/libraries/Tagster.Auth/Models/JsonWebToken.cs
@@ -4,10 +4,6 @@ namespace Tagster.Auth.Models
     {
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
-<<<<<<< HEAD
-        public string Id { get; set; }
-=======
         public int Id { get; set; }
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
     }
 }
