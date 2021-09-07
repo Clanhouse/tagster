82611c691387af16a1d63766860b3f27e1afb44f
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 12:54:58 +0200

    Update AuthDto.cs

--- TagsterWebAPI/libraries/Tagster.Auth/Dtos/AuthDto.cs
+++ TagsterWebAPI/libraries/Tagster.Auth/Dtos/AuthDto.cs
@@ -5,11 +5,7 @@ namespace Tagster.Auth.Dtos
 {
     public class AuthDto
     {
-<<<<<<< HEAD
-        public Guid Id { get; set; }
-=======
         public int Id { get; set; }
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
         public string AccessToken { get; set; }
         [JsonIgnore]
         public string RefreshToken { get; set; }
