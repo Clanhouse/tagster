bd3d83f0de3a9b66168715596dc0c0e7b1028089
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 13:08:39 +0200

    Update Tag.cs

--- TagsterWebAPI/libraries/Tagster.DataAccess/Entities/Tag.cs
+++ TagsterWebAPI/libraries/Tagster.DataAccess/Entities/Tag.cs
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-﻿using System.ComponentModel.DataAnnotations.Schema;
-=======
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
 
 namespace Tagster.DataAccess.Entities
 {
     [Table("Tags")]
-<<<<<<< HEAD
-    public record Tag {
-=======
     public record Tag
     {
         [Key]
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
         public int Id { get; set; }
         public string TagName { get; set; }
         public int ProfileId { get; set; }
