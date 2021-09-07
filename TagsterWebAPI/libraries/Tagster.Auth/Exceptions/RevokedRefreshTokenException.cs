38d2c9eddfc392d501a9df3bf5a5cb5ba3f0ea51
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 12:55:54 +0200

    Update RevokedRefreshTokenException.cs

--- TagsterWebAPI/libraries/Tagster.Auth/Exceptions/RevokedRefreshTokenException.cs
+++ TagsterWebAPI/libraries/Tagster.Auth/Exceptions/RevokedRefreshTokenException.cs
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-﻿namespace Tagster.Auth.Exceptions
-{
-    public class RevokedRefreshTokenException : AuthException
-    {
-        public override string Code { get; } = "revoked_refresh_token";
-=======
 ﻿using System.Net;
 using Tagster.Exception.Models;
 
@@ -14,7 +7,6 @@ namespace Tagster.Auth.Exceptions
     {
         public override string Code { get; } = "revoked_refresh_token";
         public override HttpStatusCode StatusCode { get; } = HttpStatusCode.BadRequest;
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
 
         public RevokedRefreshTokenException() : base("Revoked refresh token.")
         {
