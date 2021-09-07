b71099818bc0cbddedf0906e16c16fee8dacead2
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 13:10:08 +0200

    Update IRefreshTokenService.cs

--- TagsterWebAPI/libraries/Tagster.Auth/Services/IRefreshTokenService.cs
+++ TagsterWebAPI/libraries/Tagster.Auth/Services/IRefreshTokenService.cs
@@ -6,11 +6,7 @@ namespace Tagster.Auth.Services
 {
     public interface IRefreshTokenService
     {
-<<<<<<< HEAD
-        Task<string> CreateAsync(Guid userId);
-=======
         Task<string> CreateAsync(int userId);
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
         Task RevokeAsync(string refreshToken);
         Task<AuthDto> UseAsync(string refreshToken);
     }
