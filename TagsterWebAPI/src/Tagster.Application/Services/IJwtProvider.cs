54e68ca250bb19e8dadddc6e41a9367301440eb2
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 13:04:38 +0200

    Update IJwtProvider.cs

--- TagsterWebAPI/src/Tagster.Application/Services/IJwtProvider.cs
+++ TagsterWebAPI/src/Tagster.Application/Services/IJwtProvider.cs
@@ -5,10 +5,6 @@ namespace Tagster.Application.Services
 {
     public interface IJwtProvider
     {
-<<<<<<< HEAD
-        AuthDto Create(Guid userId, string email);
-=======
         AuthDto Create(int userId, string email);
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
     }
 }
