7b4c2de83bbcecd05c42e6c4e4b30f3180722d04
flapek <46303285+flapek@users.noreply.github.com>
2021-09-07 13:10:42 +0200

    Update JsonWebRefreshToken.cs

--- TagsterWebAPI/libraries/Tagster.Auth/Models/JsonWebRefreshToken.cs
+++ TagsterWebAPI/libraries/Tagster.Auth/Models/JsonWebRefreshToken.cs
@@ -6,22 +6,14 @@ namespace Tagster.Auth.Models
     public class JsonWebRefreshToken
     {
         public Guid Id { get; set; }
-<<<<<<< HEAD
-        public Guid UserId { get; set; }
-=======
         public int UserId { get; set; }
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
         public string Token { get; set; }
         public DateTime Expires { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? RevokedAt { get; private set; }
         public bool Revoked => RevokedAt.HasValue;
 
-<<<<<<< HEAD
-        public JsonWebRefreshToken(Guid id, Guid userId, string token, DateTime expires, DateTime createdAt)
-=======
         public JsonWebRefreshToken(Guid id, int userId, string token, DateTime expires, DateTime createdAt)
->>>>>>> e56f5e008b44f871bf3346eb31d0999174b491f7
         {
             Id = id;
             UserId = userId;
