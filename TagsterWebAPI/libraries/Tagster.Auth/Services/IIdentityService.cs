393e7a9642769ce51510a4e274e31d2cb72588f8
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-09-09 22:20:52 +0200

    Unwanted comments removed. Double migration has been removed

--- TagsterWebAPI/libraries/Tagster.Auth/Services/IIdentityService.cs
+++ TagsterWebAPI/libraries/Tagster.Auth/Services/IIdentityService.cs
@@ -7,7 +7,6 @@ namespace Tagster.Auth.Services
 {
     public interface IIdentityService
     {
-        //Task<UserDto> GetAsync(Guid id, CancellationToken cancellationToken);
         Task<AuthDto> SignInAsync(SignIn command, CancellationToken cancellationToken);
         Task SignUpAsync(SignUp command, CancellationToken cancellationToken);
         Task SignOutAsync(SignOut command, CancellationToken cancellationToken);
