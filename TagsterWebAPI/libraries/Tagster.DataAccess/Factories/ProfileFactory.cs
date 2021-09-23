7f1fa5cc23eb3c8cf368b5f017df59936db315d6
Flapek <filap4@gmail.com>
2021-09-23 22:24:06 +0200

    remove id from creating model

--- TagsterWebAPI/libraries/Tagster.DataAccess/Factories/ProfileFactory.cs
+++ TagsterWebAPI/libraries/Tagster.DataAccess/Factories/ProfileFactory.cs
@@ -5,8 +5,8 @@ namespace Tagster.DataAccess.Factories
 {
     public class ProfileFactory
     {
-        public static Profile Create(int id, string surname, string name, ICollection<Tag> tags)
-                => new() { Id = id, LastName = surname, Name = name, ProfileTags = tags };
+        public static Profile Create(string surname, string name, ICollection<Tag> tags)
+                => new() { LastName = surname, Name = name, ProfileTags = tags };
 
     }
 }
