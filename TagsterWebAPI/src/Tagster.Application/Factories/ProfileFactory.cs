20813483fe5b945c7f96c1f1a632ec425d85cfe6
kacperantoniak <86152585+kacperantoniak@users.noreply.github.com>
2021-09-27 20:50:25 +0200

    TAG 21 | PUT  (#18)
    * Initial data implementation idea (PUT)
    
    * Bugfixes, changes to PUT implementation
    
    * Minor improvements
    
    * remove id from creating model
    
    * add command and handler for adding tags to profile
    
    * PUT refactor (in progress)
    
    * PUT refactor
    
    1.0
    
    * clean usings
    
    * Factories fix (profile)
    
    Moved to Application
    Changed how paramaters are passed
    
    * clean usings and comments
    
    * Update appsettings.json
    
    * Removed unnecessary class
    
    Co-authored-by: Flapek <filap4@gmail.com>

---
+++ TagsterWebAPI/src/Tagster.Application/Factories/ProfileFactory.cs
@@ -0,0 +1,12 @@
+﻿using Tagster.Application.Commands.AddTagsToProfile;
+using Tagster.DataAccess.Entities;
+
+namespace Tagster.Application.Factories
+{
+    public class ProfileFactory
+    {
+        public static Profile Create(AddTagsToProfile request)
+                => new() { Name = request.Name, LastName = request.Surname, ProfileTags = request.Tags };
+
+    }
+}
