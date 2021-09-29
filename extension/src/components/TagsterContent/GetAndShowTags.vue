094b3048169c72bb48777a35b60a574dbd51d7ae
KrzysztofPa <krzysztofpawlak2000@gmail.com>
2021-09-29 19:46:54 +0200

    visual fixes, create empty function to manipulate tags

--- extension/src/components/TagsterContent/GetAndShowTags.vue
+++ extension/src/components/TagsterContent/GetAndShowTags.vue
@@ -2,15 +2,25 @@
   <div>
     <ul>
       <li class="TagsterTag" v-for="(tag, index) in tags" :key="index">
-        {{ tag.tagName }}
+        <p @dblclick="editTag($event)">{{ tag.tagName }}</p>
+        <button @click="deleteTag($event)">
+          <binIcon />
+        </button>
+      </li>
+      <li class="TagsterTag addTag" @click="addTag()">
+        <p>Click to add more tags</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import binIcon from "../svg/bin.vue";
 export default {
   name: "GetAndShowTags",
+  components: {
+    binIcon,
+  },
   data: function () {
     return {
       tags: [],
@@ -26,7 +36,15 @@ export default {
         }
       );
     },
+    deleteTag: function (event) {
+      console.log(event); //TODO: implements delete
+    },
+    editTag: function (event) {
+      console.log(event); //TODO: implements edit
+    },
+    addTag: function () {},
   },
+
   created() {
     this.getTagsFromApi();
   },
@@ -42,12 +60,32 @@ export default {
   width: 100%;
 }
 .TagsterTag {
-  background-color: grey;
-  color: white;
-  border-radius: 0.2em;
-  display: inline-block;
+  background-color: #c4c4c4;
+  color: black;
+  border-radius: 10px;
+  display: inline-flex;
   margin: 0.5em;
   padding: 0.5em;
+}
+
+.TagsterTag p {
+  cursor: pointer;
+}
+
+button {
+  margin-left: 0.5em;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.addTag {
+  background-color: #32a852;
+  color: white;
+  cursor: pointer;
+}
+.addTag p {
+  font-weight: 500;
 }
 
 .good {
