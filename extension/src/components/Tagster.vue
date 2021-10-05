2d897d3d22e23fb3e22a026e38a4752bec92ad07
KrzysztofPa <69083890+KrzysztofPa@users.noreply.github.com>
2021-10-05 17:38:10 +0200

    Tag 43 (#20)
    * visual fixes, create empty function to manipulate tags
    
    * add ShowTags.vue, refactor
    
    * fix addtags, edittags, visual fix input
    
    * add 'check', fix addNewTag

--- extension/src/components/Tagster.vue
+++ extension/src/components/Tagster.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="TagsterContainer">
     <GetAndShowTags />
-    <TagsterTagsMenu />
   </div>
 </template>
 
 <script>
 import GetAndShowTags from "./TagsterContent/GetAndShowTags.vue";
-import TagsterTagsMenu from "./TagsterContent/TagsterTagsMenu.vue";
+
 export default {
   name: "Tagster",
   components: {
     GetAndShowTags,
-    TagsterTagsMenu,
   },
   data: function () {
     return {
