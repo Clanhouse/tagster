37bf8e5ab7bae1221bc54c91ec0c4ae08e3a9eeb
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-10-21 21:43:10 +0200

    CookieFactory Tests created

--- TagsterWebAPI/librariesTests/Tagster.Exception.UnitTests/Services/CookieFactoryTests.cs
+++ TagsterWebAPI/librariesTests/Tagster.Exception.UnitTests/Services/CookieFactoryTests.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tagster.Infrastructure.Services;
 
 namespace Tagster.Exception.UnitTests.Services
 {
     public class CookieFactoryTests
     {
-
+        private readonly ICookieFactory _cookieFactory;
     }
 }
