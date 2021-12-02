27b583235517c4c2debd3326929e08456da7c42f
Flapek <filap4@gmail.com>
2021-12-02 15:56:47 +0100

    Update AdminService.cs

--- TagsterWebAPI/src/Tagster.Infrastructure/Services/AdminService.cs
+++ TagsterWebAPI/src/Tagster.Infrastructure/Services/AdminService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -8,7 +7,6 @@ using Tagster.Application.Commands.GenFakeData;
 using Tagster.Application.Factories;
 using Tagster.Application.Services;
 using Tagster.DataAccess.Models;
-using Tagster.Domain.Entities;
 using Tagster.Infrastructure.EF;
 
 namespace Tagster.Infrastructure.Services;
@@ -24,29 +22,22 @@ internal sealed class AdminService : IAdminService
         Random rand = new();
 
         if (!File.Exists(Path.Combine(AppContext.BaseDirectory, "FakeData.json")))
-        {
             throw new ArgumentException("File FakeData.json does not exist");
-        }
 
-        string json = new StreamReader(Path.Combine(AppContext.BaseDirectory, "FakeData.json")).ReadToEnd();
+        var json = new StreamReader(Path.Combine(AppContext.BaseDirectory, "FakeData.json")).ReadToEnd();
 
         var fakeData = JsonSerializer.Deserialize<FakeData>(json);
         for (int i = 0; i < request.ProfilesCount; i++)
         {
-            string name = fakeData.Names[rand.Next(fakeData.Names.Length)];
+            var name = fakeData.Names[rand.Next(fakeData.Names.Length)];
+            var surname = fakeData.Surnames[rand.Next(fakeData.Surnames.Length)];
 
-            string surname = fakeData.Surnames[rand.Next(fakeData.Surnames.Length)];
-
-            string href = fakeData.Hrefs[rand.Next(fakeData.Hrefs.Length)];
-
-            ICollection<Tag> tags = TagFactory.Create(request.MaxTagsPerProfile, fakeData);
-
-            Profile profile = ProfileFactory.Create(new AddTagsToProfile()
+            var profile = ProfileFactory.Create(new AddTagsToProfile()
             {
                 Surname = surname,
                 Name = name,
-                Tags = tags,
-                Href = href
+                Tags = TagFactory.Create(request.MaxTagsPerProfile, fakeData),
+                Href = $"{name}-{surname}"
             });
 
             await _context.Profiles.AddAsync(profile);
