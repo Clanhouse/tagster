6d1d51d017cf1da487e4a45d566fdfdf4cb85dd5
kchowanski <kc@oddslife.com>
2021-12-04 21:00:34 +0100

    TAG-41 move profile not found logic to handler

---
+++ TagsterWebAPI/src/Tagster.Application/Exceptions/ProfileNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using Tagster.Exception.Models;
+
+namespace Tagster.Application.Exceptions;
+
+public class ProfileNotFoundException : AppException
+{
+    public override string Code => "profile_not_found";
+    public override HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+
+    public ProfileNotFoundException(string href) : base($"Profile with HREF: '{href}' was not found.")
+    {
+    }
+}
