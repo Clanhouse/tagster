8aff180cfeefe4d6b23e88261f278b923b9b6425
Flapek <filap4@gmail.com>
2022-01-06 19:01:14 +0100

    Update 20220106175603_AddRoleToUser.cs

--- TagsterWebAPI/src/Tagster.Infrastructure/EF/Migrations/20220106175603_AddRoleToUser.cs
+++ TagsterWebAPI/src/Tagster.Infrastructure/EF/Migrations/20220106175603_AddRoleToUser.cs
@@ -3,38 +3,37 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Tagster.Infrastructure.EF.Migrations
+namespace Tagster.Infrastructure.EF.Migrations;
+
+public partial class AddRoleToUser : Migration
 {
-    public partial class AddRoleToUser : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "Role",
-                schema: "tagster",
-                table: "Users",
-                type: "text",
-                nullable: true);
+        migrationBuilder.AddColumn<string>(
+            name: "Role",
+            schema: "tagster",
+            table: "Users",
+            type: "text",
+            nullable: true);
 
-            migrationBuilder.InsertData(
-                schema: "tagster",
-                table: "Users",
-                columns: new[] { "Id", "CreatedAt", "Email", "Password", "Role" },
-                values: new object[] { 1, new DateTime(2022, 1, 6, 17, 56, 2, 321, DateTimeKind.Utc).AddTicks(6509), "admin@admin.com", "secret", "Admin" });
-        }
+        migrationBuilder.InsertData(
+            schema: "tagster",
+            table: "Users",
+            columns: new[] { "Id", "CreatedAt", "Email", "Password", "Role" },
+            values: new object[] { 1, new DateTime(2022, 1, 6, 17, 56, 2, 321, DateTimeKind.Utc).AddTicks(6509), "admin@admin.com", "secret", "Admin" });
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DeleteData(
-                schema: "tagster",
-                table: "Users",
-                keyColumn: "Id",
-                keyValue: 1);
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DeleteData(
+            schema: "tagster",
+            table: "Users",
+            keyColumn: "Id",
+            keyValue: 1);
 
-            migrationBuilder.DropColumn(
-                name: "Role",
-                schema: "tagster",
-                table: "Users");
-        }
+        migrationBuilder.DropColumn(
+            name: "Role",
+            schema: "tagster",
+            table: "Users");
     }
 }
