ad80a4777b65143042e47293a8818109340eb49b
flapek <filap4@gmail.com>
2022-01-12 16:25:11 +0100

    Added missing handler for refresh token

---
+++ TagsterWebAPI/src/Tagster.Application/Commands/RefreshTokens/RefreshTokensHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Tagster.Auth.Dtos;
+using Tagster.Auth.Services;
+using Tagster.CQRS.Commands.Handlers;
+
+namespace Tagster.Application.Commands.RefreshTokens;
+
+internal sealed class RefreshTokensHandler : ICommandHandler<RefreshTokens, AuthDto>
+{
+    private readonly IRefreshTokenService _refreshTokenService;
+
+    public RefreshTokensHandler(IRefreshTokenService refreshTokenService) 
+        => _refreshTokenService = refreshTokenService;
+
+    public Task<AuthDto> Handle(RefreshTokens request, CancellationToken cancellationToken) 
+        => _refreshTokenService.UseAsync(request.RefreshToken);
+}
