ff80aed1416f6a7ee66c8b51695a9ef576569a0a
flapek <46303285+flapek@users.noreply.github.com>
2022-02-16 19:14:33 +0100

    Change frontend framework (#34)
    * replace frontend framework
    
    * add config files and format code
    
    * remove unnecessary files
    
    * clear code
    
    * Update App.tsx

---
+++ extension/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Login buttons and maybe some settings here.</p>
+      </header>
+    </div>
+  );
+}
