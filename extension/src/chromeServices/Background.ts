58ab6180dba9cbb9822ae4cc344f5db6e84ae47a
Wojciech Jarosz <wojtek_j@o2.pl>
2022-03-18 14:31:14 +0100

    TAG-44: Tag choser display

--- extension/src/chromeServices/Background.ts
+++ extension/src/chromeServices/Background.ts
@@ -1,21 +1,27 @@
-import { IdsMessage, UpdateTagsRequest } from '../types';
-import { Tags } from '../types/Tags';
+import {IdsMessage, UpdateTagsRequest} from '../types';
+import {Tags} from '../types/Tags';
 import MessageSender = chrome.runtime.MessageSender;
 
 function fetchDataListener(
-  message: IdsMessage,
-  sender: MessageSender,
-  sendResponse: (response: UpdateTagsRequest) => void
+    message: IdsMessage,
+    sender: MessageSender,
+    sendResponse: (response: UpdateTagsRequest) => void
 ) {
   console.log('background.ts got message' + message);
   //TODO fetch tags here for real
   const result: Tags[] = [];
   for (let id of message.ids) {
-    result.push({ id, tags: ['tag1', 'tag 2', 'tag 3'] });
+    result.push({id, tags: ['tag1', 'tag 2', 'tag 3']});
   }
-  sendResponse({ type: 'UPDATE_TAGS', value: result });
+  sendResponse({type: 'UPDATE_TAGS', value: result});
 }
 
-chrome.runtime.onMessage.addListener(fetchDataListener);
+function isValidChromeRuntime() {
+  return chrome.runtime && !!chrome.runtime.getManifest();
+}
+
+if (isValidChromeRuntime()) {
+  chrome.runtime.onMessage.addListener(fetchDataListener);
+}
 
 export {};
--- extension/src/chromeServices/ContentScript.tsx
+++ extension/src/chromeServices/ContentScript.tsx
@@ -1,47 +1,53 @@
-import { IdsMessage, UpdateTagsRequest, UpdateTagsResponse } from '../types';
+import {IdsMessage, UpdateTagsRequest, UpdateTagsResponse} from '../types';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { Autocomplete, TextField } from '@mui/material';
+import {Autocomplete, debounce, TextField} from '@mui/material';
 
-function updateComponents(msg: UpdateTagsRequest) {}
+const components = new Map<string, JSX.Element>();
+
+function updateComponents(msg: UpdateTagsRequest) {
+}
 
 const updateTagsListener = (
-  msg: UpdateTagsRequest,
-  sender: chrome.runtime.MessageSender,
-  sendResponse: (response: UpdateTagsResponse) => void
+    msg: UpdateTagsRequest,
+    sender: chrome.runtime.MessageSender,
+    sendResponse: (response: UpdateTagsResponse) => void
 ) => {
   console.log('[content.js]. Message received', msg);
 
   updateComponents(msg);
 
-  sendResponse({ success: true });
+  sendResponse({success: true});
 };
 
 function onDomUpdate() {
-  // TODO fetch real necessary IDs
-  const ids = Array.from(document.getElementsByTagName<'h1'>('h1')).map((h1) => h1.innerText);
-
   const message: IdsMessage = {
     type: 'FETCH_IDS',
-    ids: ids,
+    ids: [],
   };
 
-  console.log('[content] sending ids: ' + ids);
-
-  chrome.runtime.sendMessage(message, (response: UpdateTagsRequest) => {
-    if (response.value) {
-      console.log('[content] got tags back! Listing:');
-      console.log(response.value);
-      response.value.forEach((tags) => console.log('id: ' + tags.id + ' tags: ' + tags.tags));
-      updateComponents(response);
-    } else {
-      console.log('[content] got empty tags');
-    }
-  });
+  setupReactComponents(getNodesToAttachTo());
+  // fetch necessary data here
+  if (1 < 0 && isValidChromeRuntime()) {
+    chrome.runtime.sendMessage(message, (response: UpdateTagsRequest) => {
+      if (response.value) {
+        updateComponents(response);
+      } else {
+        console.log('[content] got empty tags');
+      }
+    });
+  }
 }
 
-chrome.runtime.onMessage.addListener(updateTagsListener);
-const observer = new MutationObserver(onDomUpdate);
+function isValidChromeRuntime() {
+  return chrome.runtime && !!chrome.runtime.getManifest();
+}
+
+if (isValidChromeRuntime()) {
+  chrome.runtime.onMessage.addListener(updateTagsListener);
+}
+
+const observer = new MutationObserver(debounce(onDomUpdate, 400));
 observer.observe(document, {
   attributes: true,
   childList: true,
@@ -49,31 +55,77 @@ observer.observe(document, {
 });
 
 const tagsPlaceholder = [
-  { id: 'asd', name: 'Tag 1' },
-  { id: 'asd2', name: 'Tag 2' },
-  { id: 'asd3', name: 'A tag 1' },
-  { id: 'asd4', name: 'Whatever 1' },
-  { id: 'asd5', name: 'Bs 1' },
-  { id: 'asd6', name: 'Kekezaur 1' },
-  { id: 'asd7', name: 'Taggeroni 1' },
+  {id: 'asd', name: 'Tag 1'},
+  {id: 'asd2', name: 'Tag 2'},
+  {id: 'asd3', name: 'A tag 1'},
+  {id: 'asd4', name: 'Whatever 1'},
+  {id: 'asd5', name: 'Bs 1'},
+  {id: 'asd6', name: 'Kekezaur 1'},
+  {id: 'asd7', name: 'Taggeroni 1'},
 ];
 
-function setupReact() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Autocomplete
-        multiple
-        limitTags={4}
-        id='multiple-limit-tags'
-        options={tagsPlaceholder}
-        getOptionLabel={(option) => option.name}
-        defaultValue={[tagsPlaceholder[0], tagsPlaceholder[1]]}
-        renderInput={(params) => <TextField {...params} label='Tags' placeholder='Add tag...' />}
-        sx={{ width: '500px' }}
-      />
-    </React.StrictMode>,
-    document.getElementById('tagster-content-root')
-  );
+function getXpathResult(expression: string) {
+  return document.createExpression(expression)
+  .evaluate(document.getElementsByTagName("body")[0]);
 }
 
-setupReact();
+function addResults(xPathResult: XPathResult, resArray: { node: Node; link: string }[], link: string) {
+  let result = xPathResult.iterateNext();
+  while (result) {
+    resArray.push({node: result, link});
+    result = xPathResult.iterateNext();
+  }
+}
+
+function getNodesForTopCard(resArray: { node: Node, link: string }[]) {
+  const xPathResult = getXpathResult("//section[contains(@class,'top-card')]//div[contains(@class,'mt2')]");
+  addResults(xPathResult, resArray, window.location.href.substring(window.location.href.indexOf("linkedin.com") + 12));
+}
+
+function getNodesForFeedPopups(resArray: { node: Node; link: string }[]) {
+  const xPathResult = getXpathResult("//div[contains(@class, 'entity-hovercard')]");
+  const result = xPathResult.iterateNext() as Element;
+  if (result) {
+    const aElements = result.getElementsByTagName("a");
+    const divElements = result.getElementsByTagName("div");
+    if (aElements.length > 0 && divElements.length > 0) {
+      const link = aElements.item(0)!.href;
+      resArray.push({node: divElements.item(divElements.length - 1) as Node, link: link})
+    }
+  }
+}
+
+function getNodesToAttachTo() {
+  const resArray: { node: Node, link: string }[] = [];
+  //top card (main page)
+  getNodesForTopCard(resArray);
+  getNodesForFeedPopups(resArray);
+  return resArray;
+}
+
+function setupReactComponents(componentsData: { node: Node, link: string }[]) {
+  for (const componentData of componentsData) {
+    if (!components.has(componentData.link)) {
+      const divElement = document.createElement("div");
+      componentData.node.appendChild(divElement);
+      const autocompleteComponent = <Autocomplete
+          multiple
+          limitTags={4}
+          id={'multiple-limit-tags-' + Math.floor(Math.random() * 500000)}
+          options={tagsPlaceholder}
+          getOptionLabel={(option) => option.name}
+          defaultValue={[tagsPlaceholder[0], tagsPlaceholder[1]]}
+          renderInput={(params) => <TextField {...params} label='Tags'
+                                              placeholder='Add tag...'/>}
+          sx={{width: '300px'}}
+      />;
+      ReactDOM.render(
+          <React.StrictMode>
+            {autocompleteComponent}
+          </React.StrictMode>,
+          divElement
+      );
+      components.set(componentData.link, autocompleteComponent);
+    }
+  }
+}
